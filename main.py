52bbe11d00be805915ed9a12f0e11e054b81783a
Nathan Podesta <nathan.podesta@icloud.com>
2022-12-14 20:38:03 +0100

    detect green ball

--- main.py
+++ main.py
@@ -5,26 +5,33 @@ import argparse
 import imutils
 import cv2
 
+# Balle Jaune: 6.7
+
+# Balle Verte: 2.3
+sizeBall = 2.3
+## config balle verte
+lower_green = (36, 69, 110)
+upper_green = (255, 255, 255)
+
+
 def distance_to_camera(focalLength, ballWidth):
     # compute and return the distance from the maker to the camera
-    KNOWN_WIDTH = 4.27 * 0.01 * 39.37
-    return ((KNOWN_WIDTH * focalLength) / ballWidth) / 39.37
+    KNOWN_WIDTH = sizeBall * 0.01
+    return ((KNOWN_WIDTH * focalLength) / ballWidth)
 
-def init_focal_length(marker, knownDistanceMeter):
+def init_focal_length(radius, knownDistanceMeter):
     # initialize the known distance from the camera to the object, which
     # in this case is 24 inches
-    KNOWN_DISTANCE = knownDistanceMeter * 39.37
+    KNOWN_DISTANCE = knownDistanceMeter
     # initialize the known object width, which in this case, the piece of
     # paper is 12 inches wide
-    KNOWN_WIDTH = 4.27 * 0.01 * 39.37
+    KNOWN_WIDTH = sizeBall * 0.01
     # load the furst image that contains an object that is KNOWN TO BE 2 feet
     # from our camera, then find the paper marker in the image, and initialize
     # the focal length
     # image = cv2.imread("images/2ft.png")
     # marker = find_marker(image)
-    return ((marker * 2) * KNOWN_DISTANCE) / KNOWN_WIDTH
-
-
+    return ((radius * 2) * KNOWN_DISTANCE) / KNOWN_WIDTH
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the (optional) video file")
@@ -33,8 +40,7 @@ args = vars(ap.parse_args())
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
 # list of tracked points
-lower_green = (55, 55, 55)
-upper_green = (78, 255, 255)
+
 init = False
 
 # if a video path was not supplied, grab the reference
@@ -67,28 +73,19 @@ while True:
     for cnt in cnts:
         c = max(cnts, key=cv2.contourArea)
         area = cv2.contourArea(cnt)
-
         rect = cv2.minAreaRect(c)
-
         ((x, y), radius) = cv2.minEnclosingCircle(c)
-        print("rect : {0}".format(rect))
 
         if not init:
             init = True
-            focalLength = init_focal_length(radius, 2)
-            print("focal length : {0}".format(focalLength))
+            focalLength = init_focal_length(radius, 1)
 
         if radius < 60:
            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 0, 255), 2)
-           inches = distance_to_camera(focalLength, radius * 2)
+           inches = round(distance_to_camera(focalLength, radius * 2), 2)
            cv2.putText(frame, "Balle - distance: {0}".format(inches), (int(x), int(y)),
-                            cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
-           print("Balle - area: {0}".format(int(area)))
-           print("Balle - x: {0}".format(int(x)))
-           print("Balle - y: {0}".format(int(y)))
-           print("Balle - radius: {0}".format(int(radius)))
-           print("center : {0}".format(center))
-           print("focal length : {0}".format(focalLength))
+                            cv2.FONT_HERSHEY_COMPLEX, 0.5, (100, 150, 0))
+        print("inches : {0}".format(inches))
 
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
